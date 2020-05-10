ced4f6c3f27937ed7ed2b826918e35e53090a371
ayush <ayushsahu99@gmail.com>
2020-05-10 10:42:58 +0530

    Displayed Information

--- website_project/webapp/views.py
+++ website_project/webapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from webapp.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me': 'Hello my name is Ayush Sahu'}
-    return render(request, 'webapp/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list,
+                 'insert_me': 'Hello my name is Ayush Sahu'}
+    return render(request, 'webapp/index.html', context=date_dict)
