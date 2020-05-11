caf3efdf876ac0af625bfd06e49d382bb4a12876
ayush <ayushsahu99@gmail.com>
2020-05-11 18:43:58 +0530

    Diiferent view to different user

--- advanced_section/advcbv/advcbv/urls.py
+++ advanced_section/advcbv/advcbv/urls.py
@@ -20,5 +20,6 @@ from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^basic_app/', include('basic_app.urls', namespace='basic_app')),
     url(r'^$', views.IndexView.as_view())
 ]
--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -4,10 +4,16 @@ from django.http import HttpResponse
 from . import models
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
 
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
