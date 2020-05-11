41a665bafb9a4eaa903942c09283c2a5eec82d9d
ayush <ayushsahu99@gmail.com>
2020-05-11 21:16:02 +0530

    Completed CBVs

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (View, TemplateView, ListView, DetailView,
+                                  CreateView, UpdateView, DeleteView)
 from django.http import HttpResponse
 from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -17,3 +19,17 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
