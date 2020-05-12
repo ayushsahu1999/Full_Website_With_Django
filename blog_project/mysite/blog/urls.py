458922d3aa2dd4dcbb699d80fd3d7da7b18be8bc
ayush <ayushsahu99@gmail.com>
2020-05-12 23:34:44 +0530

    crud in first clone project

---
+++ blog_project/mysite/blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from basic_app import views
+
+urlpatterns = [
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
+    url(r'^drafts/$', views.DraftsListView.as_view(), name='post_draft_list'),
+]
